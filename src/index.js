3d4adeb727388deec5bdf19fe514b97c15ec56f2
Abir7109 <rahikulmakhtum147@gmail.com>
2025-11-11 16:06:35 +0600

    chore(build): bump client bundle to purge stale cached main.*.js on CDN

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Build bump to invalidate cached JS on CDN
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+
+// eslint-disable-next-line no-unused-vars
+const __BUILD_BUMP__ = '2025-11-11T10:06:30Z';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
